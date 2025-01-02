961b20bdd835d15e3bbe68e4692d07ea52814af9
drilonsaitii <drilon@websofort.de>
2025-01-02 08:52:08 +0100

    Added PageSeeder.

---
+++ application/database/seeders/PagesSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Page;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Log;
+
+class PagesSeeder extends Seeder{
+    public function run(){
+
+
+        $page = Page::where('slug','!=','/')->where('tempname','presets.default.')->get();
+        foreach ($page as $item) {
+            $item->delete();
+        }
+        Page::create(['name' => 'About us', 'slug' => '/about','secs' => json_encode(['about']),
+            'tempname' => 'presets.default.','is_default' => '0',
+            'created_at' => now(), 'updated_at' => now()]);
+        Page::create(['name' => 'Downloads', 'slug' => '/downloads','secs' => json_encode(['']),
+            'tempname' => 'presets.default.','is_default' => '0',
+            'created_at' => now(), 'updated_at' => now()]);
+
+    }
+}
